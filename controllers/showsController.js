a21070764afc04d4b41fc2ef7b5f8083fa3ace1e
Avan <mujibsardar@gmail.com>
2020-12-01 16:38:27 -0800

    basic controller done

--- controllers/showsController.js
+++ controllers/showsController.js
@@ -4,15 +4,6 @@ var uniqid = require('uniqid');
 // Import TV Show Model
 const Show = require("./../models/Show");
 
-// Get All Shows
-exports.list_shows = function(req, res) {
-  Show.find({}, function(err, shows) {
-    if (err)
-      res.send(err);
-    res.json(shows);
-  });
-};
-
 // Create New Show
 exports.create_show = function(req, res) {
   // Create new TV Show Object using the incoming data
@@ -26,6 +17,45 @@ exports.create_show = function(req, res) {
       res.send(err);
     // Otherwise send back the new tv show object
     res.json(new_tv_show);
+  });
+};
+
+// Get All Shows
+exports.list_shows = function(req, res) {
+  Show.find({}, function(err, shows) {
+    if (err)
+      res.send(err);
+    res.json(shows);
+  });
+};
+
+// Get Single Show
+exports.get_show = function(req, res) {
+  let id = req.params.id;
+  Show.find({_id:id}, function(err, show) {
+    if (err)
+      res.send(err);
+    res.json(show);
+  });
+};
+
+// Update Single Show
+exports.update_show = function(req, res) {
+  let id = req.params.id;
+  Show.findOneAndUpdate({_id:id}, req.body, function(err, show) {
+    if (err)
+      res.send(err);
+    res.json(show);
+  });
+};
+
+// Delete Show
+exports.delete_show = function(req, res) {
+  let id = req.params.id;
+  Show.deleteOne({_id:id}, function(err, data) {
+    if (err)
+      res.send(err);
+    res.send({Message: "Success", data: data});
   });
 };
 
--- routes/api.js
+++ routes/api.js
@@ -13,8 +13,6 @@ var router = express.Router();
 
 // show --> {id: 0, title: "title", platform: "platform", releaseYear: year, image: "image"}
 
-let shows = [];
-
 // ********************************************* SHOWS API (CRUD) ********************************************* //
 
 // CREATE
@@ -25,51 +23,13 @@ router.post('/', showsCtrl.create_show);
 router.get('/', showsCtrl.list_shows);
 
 // READ - Single Show
-router.get('/show/:id', function(req, res, next) {
-  // 1) Find the show in the shows array
-  let id = req.params.id;
-  let show;
-  for(let i = 0; i < shows.length; i++){
-    if(shows[i].id == id){
-      show = shows[i];
-    }
-  }
-  // 2) Send back the show that we found
-  res.send(show);
-});
+router.get('/show/:id', showsCtrl.get_show);
 
 // UPDATE Single Show
-router.put('/show/:id', function(req, res, next) {
-  // 1) Extract all the info about the show
-  let id = req.params.id;
-  let title = req.body.title;
-  let platform = req.body.platform;
-  let releaseYear = req.body.releaseYear;
-  let image = req.body.image;
-  // 2) Create new updated show info
-  let updatedShow = {id: id, title: title, platform: platform, releaseYear: releaseYear, image: image};
-  // 3) Find the show based on id
-  for(let i = 0; i < shows.length; i++){
-    if(shows[i].id == id){
-      shows[i] = updatedShow;
-    }
-  }
-  res.send(updatedShow);
-});
+router.put('/show/:id', showsCtrl.update_show);
 
 // DELETE Single Show
-router.delete('/show/:id', function(req, res, next) {
-  // 1) Extract all the info about the show
-  let id = req.params.id;
-  // 2) Find the show based on id
-  for(let i = 0; i < shows.length; i++){
-    if(shows[i].id == id){
-      // NOW DELETE THE SHOW FROM THE SHOWS ARRAY
-      shows.splice(i, 1);
-    }
-  }
-  res.send(shows);
-});
+router.delete('/show/:id', showsCtrl.delete_show);
 
 // DELETE - Delete All Shows
 router.delete('/', showsCtrl.delete_shows);
